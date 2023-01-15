11079beb721ec8a169648fac057268a9a065af98
Eshaq Jamdar <eshaqjamdar@pop-os.localdomain>
2023-01-14 17:23:49 -0800

    added public dir and code clean up

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,33 +1,38 @@
 import express from 'express'
 import logger from '../../utilities/logger'
+const fs = require('fs')
 let path = require("path");
 const sharp = require('sharp');
 const images = express.Router();
 images.get('/', logger, (req, res)=>{
     const fileName = req.query.filename as string;
     const width = req.query.width as string;
-    const height = req.query.width as string;
-    if(fileName){
-        console.log(fileName)
-        const fileArr: String[] = fileName.split('.')
-        const thumbFileName: string = `assets/thumb/${fileArr[0]}_thumb.${fileArr[1]}`
+    const height = req.query.height as string;
+
+    const fileArr: String[] = fileName.split('.')
+    const thumbFileName: string = `../../../public/assets/thumb/${fileArr[0]}_thumb_${width}x${height}.${fileArr[1]}`
+    const thumbnailPath = path.join(__dirname, `../../../public/assets/full/${fileName}`)
+
+    if(fs.existsSync(thumbnailPath)){
+        res.sendFile(thumbnailPath)
+    }
+    else if(fileName && width && height){
         resizeImage(fileName, parseInt(width), parseInt(height), thumbFileName)
-        res.sendFile(__dirname+"/"+thumbFileName);
+        res.sendFile(thumbnailPath);
     }else{
-        res.send("Error: No image input was given.")
-        console.log(fileName)
+        res.send("Error: No image input was given or the parameters (width and height) are missing.")
     }
 });
 
 async function resizeImage(fileName:String, width: number, height: number, result:String) {
     try {
-        console.log(__dirname )
-      await sharp(`${__dirname}/assets/full/${fileName}`, { root : __dirname}) 
+        const fullSizePath = `../../../public/assets/full/${fileName}`
+      await sharp(path.join(__dirname, fullSizePath)) 
         .resize({
           width: width,
           height: height
         })
-        .toFile(`${__dirname}/${result}`)
+        .toFile(path.join(__dirname, result))
     } catch (error) {
       console.log(error);
     }
